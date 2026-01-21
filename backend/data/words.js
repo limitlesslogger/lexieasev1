17f1b12bb9d098eabf5e509c9b96d0a577aa7a40
Meera <meeraaiswar@gmail.com>
2026-01-21 21:24:02 +0530

    Added words level and tested successfully.

---
+++ backend/data/words.js
@@ -0,0 +1,26 @@
+export const WORDS = [
+  {
+    id: "w1",
+    text: "apple",
+  },
+  {
+    id: "w2",
+    text: "elephant",
+  },
+  {
+    id: "w3",
+    text: "water",
+  },
+  {
+    id: "w4",
+    text: "book",
+  },
+  {
+    id: "w5",
+    text: "school",
+  },
+  {
+    id: "w6",
+    text: "beautiful",
+  },
+];
